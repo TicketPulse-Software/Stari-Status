b80eedfcae440361c5b4b138f51ffdb4f6e7c291
Luc <67204364+YEET200@users.noreply.github.com>
2024-07-04 16:24:31 -0400

    Create login.php
    Signed-off-by: Luc <67204364+YEET200@users.noreply.github.com>

---
+++ admin/login.php
@@ -0,0 +1,35 @@
+<?php
+require '../includes/db.php';
+require '../includes/auth.php';
+
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if (login($pdo, $username, $password)) {
+        header('Location: index.php');
+        exit();
+    } else {
+        $error = 'Invalid username or password';
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <?php if ($error): ?>
+        <p style="color:red;"><?php echo htmlspecialchars($error); ?></p>
+    <?php endif; ?>
+    <form method="POST" action="">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
